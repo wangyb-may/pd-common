51a3c3a26ab0fb6d5c78902cfb8ba62efbe8811c
王韵博 <wangyb@111.com>
2020-07-17 15:53:12 +0800

    屏蔽oss地址

--- src/main/java/com/bysj/wyb/common/service/SysService.java
+++ src/main/java/com/bysj/wyb/common/service/SysService.java
@@ -36,11 +36,11 @@ public class SysService {
         System.out.println(uploadCatalogAndName);
         HandleResult hr=new HandleResult();
         // Endpoint以杭州为例，其它Region请按实际情况填写。
-        String endpoint = "http://oss-cn-chengdu.aliyuncs.com";
+        String endpoint = "你的oss对象地址";
         // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-        String accessKeyId = "LTAI4FqEfb86cMQiGtcEtxPn";
-        String accessKeySecret = "RxEK1zRbOaCMWc78NNNpK82LUqom89";
-        String bucketName = "wyb-bysj";
+        String accessKeyId = "授权id";
+        String accessKeySecret = "授权密码";
+        String bucketName = "项目名称";
         URL url = null;
 
         // 创建OSSClient实例。
