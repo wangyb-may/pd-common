22c871906c187ff8adca5d633ea53dec6f760bb9
wangyb-may <62542756+wangyb-may@users.noreply.github.com>
2020-11-10 10:08:51 +0800

    Update SysService.java

--- src/main/java/com/bysj/wyb/common/service/SysService.java
+++ src/main/java/com/bysj/wyb/common/service/SysService.java
@@ -38,8 +38,8 @@ public class SysService {
         // Endpoint以杭州为例，其它Region请按实际情况填写。
         String endpoint = "http://oss-cn-chengdu.aliyuncs.com";
         // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-        String accessKeyId = "LTAI4FqEfb86cMQiGtcEtxPn";
-        String accessKeySecret = "RxEK1zRbOaCMWc78NNNpK82LUqom89";
+        String accessKeyId = "your accessKeyId";
+        String accessKeySecret = "your accessKeySecret";
         String bucketName = "wyb-bysj";
         URL url = null;
 
